85c562c440f1499a5f40de997b5cc259ea7bc093
Eduardo Dantas <eduardo.dantas@ascential.com>
2022-08-05 00:35:22 -0300

    teste

--- scripts.js
+++ scripts.js
@@ -12,6 +12,7 @@ $(document).ready(()=>{
             <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
         `
     }
+
     corpo.innerHTML = elementosHTML()
     function renderizar(){
         let titulo = document.getElementById("titulo")
